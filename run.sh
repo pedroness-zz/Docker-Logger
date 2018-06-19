829f1d7b5c1da483f9f02df273efe1fe00200f92
Pedro <pedro@waxedmobile.com>
2018-06-19 17:01:07 +0200

    Path fswatch to node file in bash

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@
 
 docker rm -f docker-logger-container
 rm -r $(pwd)/app/json
+
 mkdir $(pwd)/app/json
 CONTAINERS=()
 
@@ -27,9 +28,13 @@ do
     CONTAINERPATHS[$ARRAY_COUNTER]=$(docker inspect --format='{{.LogPath}}' ${CONTAINERS[$ARRAY_COUNTER]})
     JSONCONTENT=${JSONCONTENT}',"'${CONTAINERS[$ARRAY_COUNTER]}'"'
     DOCKERVOLUMECOMMAND=${DOCKERVOLUMECOMMAND}' -v '${CONTAINERPATHS[$ARRAY_COUNTER]}':/home/node/app/json/'${CONTAINERS[$ARRAY_COUNTER]}'/logs.json'
+    FSCOMMAND=${FSCOMMAND}'fswatch -xr /home/node/app/json/'${CONTAINERS[$ARRAY_COUNTER]}'/logs.json | xargs -I {}  node /home/node/app/monitor.js {} & '
     ARRAY_COUNTER=$(($ARRAY_COUNTER + 1))
 done
 
 echo '['${JSONCONTENT:1}']' > ./app/json/containers.json
-
-docker run -it -v $(pwd)/app:/home/node/app ${DOCKERVOLUMECOMMAND} -p 706:706 --name docker-logger-container docker-logger http-server -d false --push-state --cors -p 706
+echo docker exec -it docker-logger-container bash -c "'"${FSCOMMAND::-3}"'" >> fswatch.sh
+chmod +x fswatch.sh
+docker run -itd -v $(pwd)/app:/home/node/app ${DOCKERVOLUMECOMMAND} -p 706:706 --name docker-logger-container docker-logger http-server -d false --push-state --cors -p 706 
+./fswatch.sh
+rm fswatch.sh
